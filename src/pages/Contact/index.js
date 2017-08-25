63086df600bab2c20d1564576b07eb13ae08cf51
Wouter Raateland <wouterraateland@gmail.com>
2017-08-25 14:10:42 +0200

    Update overall style and update contact information section

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -7,9 +7,20 @@ const Contact = () => (
       <div className="row">
         <div className="col-12">
           <h2>Contact information</h2>
-          <p><a href="mailto:wouterraateland@gmail.com">wouterraateland@gmail.com</a></p>
-          <p><a href="tel:+31648271371">+31 6 48 27 13 71</a></p>
-          <p><a href="http://wouterraateland.nl">wouterraateland.nl</a></p>
+        </div>
+        <div className="col-6">
+          <a className="Contact-social email" href="mailto:wouterraateland@gmail.com">
+            <span>wouterraateland@gmail.com</span>
+          </a>
+        </div>
+        <div className="col-6">
+          <a className="Contact-social telephone" href="tel:+31648271371">+31 6 48 27 13 71</a>
+        </div>
+        <div className="col-6">
+          <a className="Contact-social website" href="http://wouterraateland.nl">wouterraateland.nl</a>
+        </div>
+        <div className="col-6">
+          <a className="Contact-social linkedin" href="https://linkedin.com/in/wouterraateland">wouterraateland</a>
         </div>
       </div>
     </div>
