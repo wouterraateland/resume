de2c53c8380c4bca1957bd6141754d18ae6ac412
Wouter Raateland <wouterraateland@gmail.com>
2017-08-25 17:11:23 +0200

    Add descriptions to projects and fix link color

--- src/components/Entry/index.js
+++ src/components/Entry/index.js
@@ -3,12 +3,15 @@ import './styles.css'
 
 import Thumbnail from 'components/Thumbnail'
 
-const Entry = ({ image, org, start, end, location, title, children }) => (
+const Entry = ({ image, org, start, end, location, title, description, children }) => (
   <div className={`Entry row${image ? ` has-image` : ``}`}>
     { image ? <Thumbnail image={image} alt={org} /> : `` }
     { org ? <h3>{org} <em>({start}{end ? ` - ${end}` : ``})</em></h3> : `` }
     { location || title
       ? <p className="tagline">{location ? `${location}. ` : ``}{title}</p>
+      : `` }
+    { description
+      ? <p className="Entry-description">{description}</p>
       : `` }
     {children
       ? <div className="Entry-projects row">{children}</div>
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -19,15 +19,18 @@ const Projects = () => (
       <Entry
         image={driebroek}
         org="Driebroek" start="2017" end="now"
-        title="Clothing brand for Aladdin pants" />
+        title="Clothing brand for Aladdin pants"
+        description="Driebroek is my newest venture aiming to produce the best fitting Aladdin pants ever. Currently the first few are sold and I am working on an online infrastructure and presence of the company." />
       <Entry
         image={parkourdelft}
         org="Parkour Delft" start="2016"
-        title="Organiser and trainer" />
+        title="Organiser and trainer"
+        description="For half a year I gave parkour training to students in a gym in Delft. Together with a friend we arranged the hall, invited people over there to train and had fun." />
       <Entry
         image={pilsmeister}
         org="Pilsmeister" start="2015" end="2016"
-        title="Web and mobile app" />
+        title="Webapp for students to tally their beers (currently offline)"
+        description="I originally created Pilsmeister for my dorm as an alternative to the tallying of beers on the fridge. The app featured direct updates using sockets, social network logins and group formation methods using QR codes. The app worked, but proved not to be more effective than classic tallying on the fridge." />
     </div>
   </div>
 )
