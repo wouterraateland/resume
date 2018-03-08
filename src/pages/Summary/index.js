3cb8a2112adedf54e34bcc88e72d62385ffad4de
Wouter Raateland <wouterraateland@gmail.com>
2018-03-08 19:14:28 +0100

    Minor fixes and additions

--- src/pages/Summary/index.js
+++ src/pages/Summary/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './styles.css'
 
 const Summary = () => (
   <section className="Summary">
     <h2>Summary</h2>
-    <p>Enthousiastic software developer with +5 years of experience. Finishing BSc. in Computer Science July 2018. Currently working part-time and organising a healtcare symposium. Applying for the position of <strong>Software engineer</strong></p>
+    <p>Enthousiastic software developer with +5 years of experience. Currently finishing my BSc. in Computer Science, working part-time and organising a healtcare symposium. Applying for <strong>Software engineer</strong></p>
   </section>
 )
 
