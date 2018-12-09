d68bc4ea9340ce6a416b8a4ea1c04da8e09ec7da
Wouter Raateland <wouterraateland@gmail.com>
2018-12-09 18:00:32 +0100

    Setup for gatsby generator

--- src/pages/Education/index.js
+++ src/pages/Education/index.js
@@ -11,19 +11,22 @@ const Education = () => (
     <h2>Education</h2>
     <Entry
       image={tud}
-      org="Delft University of Technology" start="2015" end="present"
+      org="Delft University of Technology" start="2015" end="2018"
       location="Delft, Netherlands"
-      title="BSc. Computer Science" />
+      title="BSc. Computer Science (8.2 GPA)"
+    />
     <Entry
       image={ebs}
       org="European Bartender School" start="2016"
       location="Dublin, Ireland"
-      title="Professional level bartender" />
+      title="Professional level bartender"
+    />
     <Entry
       image={tud}
       org="Delft University of Technology" start="2014" end="2015"
       location="Delft, Netherlands"
-      title="BSc. Applied Mathematics, First year (8.3 GPA)" />
+      title="BSc. Applied Mathematics, First year (8.3 GPA)"
+    />
   </section>
 )
 
