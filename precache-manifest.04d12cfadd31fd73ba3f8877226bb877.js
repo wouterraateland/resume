b5098c74d38e58f0a57870fff34a1a5ac1960665
Wouter Raateland <wouterraateland@gmail.com>
2018-12-21 22:04:49 +0100

    Updates

---
+++ precache-manifest.04d12cfadd31fd73ba3f8877226bb877.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "712a912627039d703add",
+    "url": "/resume/static/css/main.a4d2d795.chunk.css"
+  },
+  {
+    "revision": "712a912627039d703add",
+    "url": "/resume/static/js/main.712a9126.chunk.js"
+  },
+  {
+    "revision": "3620edcef93dd523697a",
+    "url": "/resume/static/js/1.3620edce.chunk.js"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "2174f335c0118397a936a7edf7711e74",
+    "url": "/resume/index.html"
+  }
+];
